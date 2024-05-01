f37116c3e5a537fc53d99cf64d4ffb73f03db67c
new name <newsrk800@gmail.com>
2024-05-01 20:17:19 +0500

    update

--- src/components/Preloaders/Preloader.jsx
+++ src/components/Preloaders/Preloader.jsx
@@ -3,9 +3,20 @@ import Style from "../../styles/components/preloaders/preloader/preloader.module
 // прелоадер для прогрузки прежде чем показать контент
 const Preloader = () => {
   return (
-    <div style={{ width: "100%" }}>
-      <div className={Style.linear_activity}>
-        <div className={Style.indeterminate}></div>
+    <div className={Style.loader_pos}>
+      <div className={Style.loader}>
+        <div className={Style.bar1}></div>
+        <div className={Style.bar2}></div>
+        <div className={Style.bar3}></div>
+        <div className={Style.bar4}></div>
+        <div className={Style.bar5}></div>
+        <div className={Style.bar6}></div>
+        <div className={Style.bar7}></div>
+        <div className={Style.bar8}></div>
+        <div className={Style.bar9}></div>
+        <div className={Style.bar10}></div>
+        <div className={Style.bar11}></div>
+        <div className={Style.bar12}></div>
       </div>
     </div>
   );
